276dfebabe46fcf7a647f1d275b793fbd3bdb8f6
Avidan Gerber <avidang@rapyd.net>
2024-11-06 14:43:30 +0200

    Refactor build script in package.json to include pnpm install step Refactor .gitignore to remove /pkg directory because it generates its own .gitignore with * every time it builds Refactor add function to accept and return i32 instead of usize Update start script in package.json for wasm-node Update project structure and dependencies

--- wasm-node/src/main.ts
+++ wasm-node/src/main.ts
@@ -1,3 +1,16 @@
 import { add } from "wasm-rust";
 
 console.log(`Result of add(1, 2): ${add(1, 2)}`);
+
+type isExact<T, U> = T extends U ? (U extends T ? true : false) : false;
+type Assert<T extends true> = T;
+
+/**
+ * Type alias `C` is used to assert that the type of webAssembly function `add` is exactly 
+ * a function that takes two numbers as arguments and returns a number.
+ * 
+ * This is done using the `Assert` utility type and the `isExact` type 
+ * predicate. If `add` type does not match the specified function type, a 
+ * type error will be raised.
+ */
+type C = Assert<isExact<typeof add, (a: number, b: number) => number>>;
