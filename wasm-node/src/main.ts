cbf00c915f165e08aa65868e8a0d558ecf181d6b
Avidan Gerber <avidang@rapyd.net>
2024-11-06 16:40:49 +0200

    Refactor Biome configuration in wasm-node
    - Updated the Biome configuration in wasm-node/biome.json to include additional rules for correctness and complexity.
    - Modified the include path in wasm-node/biome.json to target the correct source files.
    
    Refactor main.ts in wasm-node
    
    - Removed type assertion comments in main.ts.
    - Replaced type assertion with explicit variable declaration in main.ts.
    
    Add workspace configuration files
    
    - Added workspace configuration files for wasm-node and wasm-rust.

--- wasm-node/src/main.ts
+++ wasm-node/src/main.ts
@@ -1,6 +1,6 @@
 import { add } from "wasm-rust";
-import type { Assert, isExact } from "./utils";
 
-console.log(`Result of add(1, 2): ${add(1, 2)}`);
+const left = 1;
+const right = 2;
 
-type C = Assert<isExact<typeof add, (a: number, b: number) => number>>;
+console.log(`Result of add(${left}, ${right}): ${add(left, right)}`);
