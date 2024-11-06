dc04882486b950d6be27b4bd0e457348db6abf3d
Avidan Gerber <avidang@rapyd.net>
2024-11-06 17:51:50 +0200

    Refactor main.ts to add support for adding a list of numbers
    This commit refactors the main.ts file in the wasm-node project to include support for adding a list of numbers using the newly added `addList` function from the wasm-rust library. It also adds an example usage of the `addList` function by adding an array of numbers and logging the result.

--- wasm-node/src/main.ts
+++ wasm-node/src/main.ts
@@ -1,6 +1,11 @@
-import { add } from "wasm-rust";
+import { add, addList } from "wasm-rust";
 
 const left = 1;
 const right = 2;
 
+const array = [1, 2, 3, 4, 5];
+
 console.log(`Result of add(${left}, ${right}): ${add(left, right)}`);
+console.log(
+	`Result of addList([${array}]): ${addList(new Float64Array(array))}`,
+);
--- wasm-rust/src/lib.rs
+++ wasm-rust/src/lib.rs
@@ -1,6 +1,11 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(left: i32, right: i32) -> i32 {
+pub fn add(left: f64, right: f64) -> f64 {
     left + right
+}
+
+#[wasm_bindgen(js_name = addList)]
+pub fn add_list(list: Vec<f64>) -> f64 {
+    list.iter().sum()
 }
