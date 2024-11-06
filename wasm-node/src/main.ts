20c52549971bb9906985e9e18a183a39892a2c64
Avidan Gerber <avidang@rapyd.net>
2024-11-06 17:55:35 +0200

    Refactor main.ts to use sumList instead of addList for adding a list of numbers

--- wasm-node/src/main.ts
+++ wasm-node/src/main.ts
@@ -1,4 +1,4 @@
-import { add, addList } from "wasm-rust";
+import { add, sumList } from "wasm-rust";
 
 const left = 1;
 const right = 2;
@@ -7,5 +7,5 @@ const array = [1, 2, 3, 4, 5];
 
 console.log(`Result of add(${left}, ${right}): ${add(left, right)}`);
 console.log(
-	`Result of addList([${array}]): ${addList(new Float64Array(array))}`,
+	`Result of sumList([${array}]): ${sumList(new Float64Array(array))}`,
 );
--- wasm-rust/src/lib.rs
+++ wasm-rust/src/lib.rs
@@ -5,7 +5,7 @@ pub fn add(left: f64, right: f64) -> f64 {
     left + right
 }
 
-#[wasm_bindgen(js_name = addList)]
-pub fn add_list(list: Vec<f64>) -> f64 {
+#[wasm_bindgen(js_name = sumList)]
+pub fn sum_list(list: Vec<f64>) -> f64 {
     list.iter().sum()
 }
